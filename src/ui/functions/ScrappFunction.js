a20cccc7157269ae54f96516061e992caf4cf3f8
Erwin Marte <erwin.mdq@gmail.com>
2023-03-11 13:39:15 -0300

    Recontruction

--- src/ui/functions/ScrappFunction.js
+++ src/ui/functions/ScrappFunction.js
@@ -3,7 +3,8 @@
 const puppeteer = require('puppeteer');
 const search = document.getElementById("form_submit");
 // TEST new url
-const url = "https://www.laanonimaonline.com/"; /* https://previewlaol.laanonimaws.com/buscar?pag=1&clave=2885384 */
+//const url = "https://www.laanonimaonline.com/";
+const url =  "https://previewlaol.laanonimaws.com/"
 let to_convert = [[]];
 // codigos de ejemplo busqueda 2974400 2974459 2974484 2974514 2806435, 2144037,2885384
 
@@ -18,7 +19,7 @@ async function la_scrap() {
     if (container.hasChildNodes()) {
         container.querySelectorAll('*').forEach(element_inside_container => element_inside_container.remove());
     }
-    //Clear the array container (to_convert) for delete last searched items (if there is any)
+    // Clear the array container (to_convert) for delete last searched items (if there is any)
     if(to_convert[0].length > 0){
         to_convert[0] = [];
     }
@@ -36,7 +37,7 @@ async function la_scrap() {
             defaultViewport: null
         });
         const page = await browser.newPage();
-        await page.goto(url, {waitUntil: 'load', timeout: 0});
+        await page.goto(url, /*{waitUntil: 'load', timeout: 0}*/);
 
         try{
             await page.click("#ModalCodigoPostal > div.modal-wrapper.posicion_fija.ingresar-codigo-postal > span");
@@ -44,7 +45,7 @@ async function la_scrap() {
             void e
         }
         await page.waitForSelector("#buscar");
-        await page.type("#buscar", article_code_to_search, {delay: 25});
+        await page.type("#buscar", article_code_to_search, {delay: 5});
         await page.keyboard.press("Enter");
 
         // Enter on the URL of the specific prodct
@@ -53,104 +54,98 @@ async function la_scrap() {
         }catch(e){
             void e
         }
+    
+        // go to speccific URL product to scrapp
+        let all_the_links = await page.$$eval('a', as => as.map(a => a.href).filter(link => link.includes('art_')));
+        let Links_witch_contain_article = Array.from(new Set(all_the_links));
+        let [unique_link_to_product] = Links_witch_contain_article;
+        await page.goto(unique_link_to_product);
 
-        //Testing to solve error when codart doesnt exist on the website
-        const selector_to_wait = "#maq_cuerpo > div.maq_col_2 > div.caja1.producto";
-        const element_to_click = await page.waitForSelector(selector_to_wait, {timeout: 1500 }).catch(() => null);
+        try{
+            await page.click("#ModalCodigoPostal > div.modal-wrapper.posicion_fija.ingresar-codigo-postal > span");
+        }catch(e){
+            void e
+        }
+        
+        ////////////////////////////////////////// Get all the information needed: //////////////////////////////////////////
 
-        if(element_to_click){
-            await page.click("#maq_cuerpo > div.maq_col_2 > div.caja1.producto > div > div");
-
-            try{
-                await page.click("#ModalCodigoPostal > div.modal-wrapper.posicion_fija.ingresar-codigo-postal > span");
-            }catch(e){
-                void e
-            }
-            //await page.waitForSelector('#cont_producto > div.clearfix.valign.spa_bot > h1');
+        // Link/Url
+        let link = page.url();
+        // Product code on the page, needed to search the images
+        let pattern_to_get_codArt = /art_(.+)\//;
+        let match = link.match(pattern_to_get_codArt);
+        let result = match[1];
+        // Images > obtein images, delete images that doesnt contain de product code then delete the duplicates
+        let imgSrc = await page.$$eval("img", allimg => allimg.map((val)=> val.getAttribute("src")));
+        let regex = new RegExp(result);
+        let all_imgs_filtered = imgSrc.filter(item => regex.test(item));
+        // Delete duplicates images
+        for (let i = 0; i < all_imgs_filtered.length; i++) {
+            // Compare the last six characters to the current string, if this is repeated anytime, this remove with splice
+            let current_letters = all_imgs_filtered[i];
+            let last_six_letters = current_letters.slice(-6);
             
-            // Get all the information needed:
-            
-            // Link/Url
-            let link = page.url();
-            console.log(link);
-            // Product code on the page, needed to search the images
-            let pattern_to_get_codArt = /art_(.+)\//;
-            let match = link.match(pattern_to_get_codArt);
-            let result = match[1];
-            // Images > obtein images, delete images that doesnt contain de product code then delete the duplicates
-            let imgSrc = await page.$$eval("img", allimg => allimg.map((val)=> val.getAttribute("src")));
-            let regex = new RegExp(result);
-            let all_imgs_filtered = imgSrc.filter(item => regex.test(item));
-            // Delete duplicates images
-            for (let i = 0; i < all_imgs_filtered.length; i++) {
-                // Compare the last six characters to the current string, if this is repeated anytime, this remove with splice
-                let current_letters = all_imgs_filtered[i];
-                let last_six_letters = current_letters.slice(-6);
-                
-                for (let j = 0; j < all_imgs_filtered.length; j++) {
-                    if (i !== j && all_imgs_filtered[j].includes(last_six_letters)) {
-                        all_imgs_filtered.splice(i, 1);
-                        i--;
-                        break;
-                    }
+            for (let j = 0; j < all_imgs_filtered.length; j++) {
+                if (i !== j && all_imgs_filtered[j].includes(last_six_letters)) {
+                    all_imgs_filtered.splice(i, 1);
+                    i--;
+                    break;
                 }
-            };
-            
-            let title = await page.evaluate(() => document.querySelector('#cont_producto > div.clearfix.valign.spa_bot > h1').textContent);
-            try{          // May have description or not, that why the "try"
-                          // Deberia eliminar los divs de descripcion, los cuales tengan "-"
-                var descriptcion = await page.evaluate(() => document.querySelector("#cont_producto > div.cont_images_productos.clearfix > div.texto.texto-light.color000000.font-size-16").textContent)
-            }catch(e){
-                void e
-            };
-            let article_code = await page.evaluate(() => document.querySelector("#cont_producto > div.cont_images_productos.clearfix > div.colorC4C4C4.font-size-12").textContent);
-            article_code = article_code.slice(5);
-            
-            // Push items to a single array to convert XLSX  and End puppeteer
-            await browser.close();
-            
-            //Push images, then converto single array, so to xlsx file can add the info to the file, then delete all the images thats that contains more than _5.jpg because only need 5 images extras, also remmove elements that doesnt contain "cdnlaol"
-            let images_to_push=[];
-            all_imgs_filtered = all_imgs_filtered.filter((url_images_check) => {
-                return url_images_check.includes("cdnlaol");
-            });
-            all_imgs_filtered.forEach(image => {
-                images_to_push.push(image);
-            });
-            let img_to_string = images_to_push.join(",");
-            let index = img_to_string.lastIndexOf("_5.jpg");
-            if (index !== -1) {
-                img_to_string = img_to_string.substring(0, index + 6);
             }
-            // Now, separate the first image, and the rest of the images, for separated in columns in the xlsx file
-            let firstCommaIndex = img_to_string.indexOf(", ");
-            let first_img = img_to_string.substring(0, firstCommaIndex);
-            let rest_of_the_imgs = img_to_string.substring(firstCommaIndex + 1);
-            
-            //push all the information scrappet so it can be showed on the dom and add latter on the xlsx file
-            to_convert[0].push([link, title, descriptcion, article_code, first_img, rest_of_the_imgs]);
-            
-            // testing ui loader
-            document.getElementById("loaders").style.display = "none";
-                    
-            // Show on the DOM the XLSX data
-            let table = document.createElement("table");
-            for (let i = 0; i < to_convert[0].length; i++) {
-                let row = document.createElement("tr");
-                for (let j = 0; j < to_convert[0][i].length; j++) {
-                    let cell = document.createElement("td");
-                    cell.innerHTML = to_convert[0][i][j];
-                    row.appendChild(cell);
-                };
-                table.appendChild(row);
-            };
-            document.getElementById("incoming-data_container").appendChild(table);
-            console.log(to_convert[0]);
-
-        }else{
-            console.log(`Error: Element ${selector_to_wait} not found on the page. For the Article with code ${article_code_to_search}`);
-            console.log(`Unable to click on the element.`)
         };
+        
+        let title = await page.evaluate(() => document.querySelector("#cont_producto > div.clearfix.valign.spa_bot > h1").textContent);
+        try{          // May have description or not, that why the "try"
+                      // Deberia eliminar los divs de descripcion, los cuales tengan "-"
+            var descriptcion = await page.evaluate(() => document.querySelector("#cont_producto > div.cont_images_productos.clearfix > div.texto.texto-light.color000000.font-size-16").textContent)
+        }catch(e){
+            void e
+        };
+        let article_code = await page.evaluate(() => document.querySelector("#cont_producto > div.cont_images_productos.clearfix > div.colorC4C4C4.font-size-12").textContent);
+        article_code = article_code.slice(5);
+        
+        // Push items to a single array to convert XLSX  and End puppeteer
+        await browser.close();
+        
+        //Push images, then converto single array, so to xlsx file can add the info to the file, then delete all the images thats that contains more than _5.jpg because only need 5 images extras, also remmove elements that doesnt contain "cdnlaol"
+        let images_to_push=[];
+        all_imgs_filtered = all_imgs_filtered.filter((url_images_check) => {
+            return url_images_check.includes("cdnlaol");
+        });
+        all_imgs_filtered.forEach(image => {
+            images_to_push.push(image);
+        });
+        let img_to_string = images_to_push.join(",");
+        let index = img_to_string.lastIndexOf("_5.jpg");
+        if (index !== -1) {
+            img_to_string = img_to_string.substring(0, index + 6);
+        }
+        // Now, separate the first image, and the rest of the images, for separated in columns in the xlsx file
+        let firstCommaIndex = img_to_string.indexOf(", ");
+        let first_img = img_to_string.substring(0, firstCommaIndex);
+        let rest_of_the_imgs = img_to_string.substring(firstCommaIndex + 1);
+        
+        //push all the information scrappet so it can be showed on the dom and add latter on the xlsx file
+        to_convert[0].push([link, title, descriptcion, article_code, first_img, rest_of_the_imgs]);
+        
+        // testing ui loader
+        document.getElementById("loaders").style.display = "none";
+                
+        // Show on the DOM the XLSX data
+        let table = document.createElement("table");
+        for (let i = 0; i < to_convert[0].length; i++) {
+            let row = document.createElement("tr");
+            for (let j = 0; j < to_convert[0][i].length; j++) {
+                let cell = document.createElement("td");
+                cell.innerHTML = to_convert[0][i][j];
+                row.appendChild(cell);
+            };
+            table.appendChild(row);
+        };
+        document.getElementById("incoming-data_container").appendChild(table);
+
+        // Look information obteined:
+        console.log(to_convert[0]);
     });
 };
 
